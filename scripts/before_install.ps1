cbe0a17e2625b1a6b5c71a8061a477ed484db049
Abdul Rafay <rafay@gmail.com>
2025-07-19 13:24:25 +0500

    update layout file

---
+++ scripts/before_install.ps1
@@ -0,0 +1,8 @@
+﻿$path = "C:\inetpub\wwwroot\StudentManagmentSystem"
+
+if (Test-Path $path) {
+    Write-Host "Removing all contents in $path..."
+    Remove-Item "$path\*" -Recurse -Force
+} else {
+    Write-Host "$path does not exist. Skipping deletion."
+}
